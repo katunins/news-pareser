9fe2a292b3304bb9968867fb607c3e390e7936be
Pavel Katunin <katunin.pavel@gmail.com>
2025-07-12 08:20:50 +0300

    firefox

--- src/stages/auth.ts
+++ src/stages/auth.ts
@@ -1,19 +1,48 @@
 import { Browser, Builder, By, until } from 'selenium-webdriver'
 import { Options as ChromeOptions } from 'selenium-webdriver/chrome'
+import { Options as FirefoxOptions } from 'selenium-webdriver/firefox'
 
 export async function openMediumAndClickStartReading() {
-    const chromeOptions = new ChromeOptions();
-    chromeOptions.addArguments('--headless');
-    chromeOptions.addArguments('--no-sandbox');
-    chromeOptions.addArguments('--disable-dev-shm-usage');
-    chromeOptions.addArguments('--disable-gpu');
-    chromeOptions.addArguments('--remote-debugging-port=9222');
-    chromeOptions.addArguments('--user-data-dir=/tmp/chrome-user-data');
+    // Пробуем сначала Chrome, если не получится - Firefox
+    let driver;
 
-    const driver = await new Builder()
-        .forBrowser('chrome')
-        .setChromeOptions(chromeOptions)
-        .build();
+    try {
+        console.log('Пробуем запустить Chrome...');
+        const chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments('--headless');
+        chromeOptions.addArguments('--no-sandbox');
+        chromeOptions.addArguments('--disable-dev-shm-usage');
+        chromeOptions.addArguments('--disable-gpu');
+        chromeOptions.addArguments('--disable-extensions');
+        chromeOptions.addArguments('--disable-plugins');
+        chromeOptions.addArguments('--disable-images');
+        chromeOptions.addArguments('--disable-javascript');
+        chromeOptions.addArguments('--disable-web-security');
+        chromeOptions.addArguments('--allow-running-insecure-content');
+        chromeOptions.addArguments('--disable-background-timer-throttling');
+        chromeOptions.addArguments('--disable-backgrounding-occluded-windows');
+        chromeOptions.addArguments('--disable-renderer-backgrounding');
+        chromeOptions.addArguments('--disable-features=TranslateUI');
+        chromeOptions.addArguments('--disable-ipc-flooding-protection');
+        chromeOptions.addArguments('--user-data-dir=/tmp/chrome-user-data-' + Date.now());
+        chromeOptions.addArguments('--remote-debugging-port=0');
+
+        driver = await new Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(chromeOptions)
+            .build();
+        console.log('Chrome запущен успешно!');
+    } catch (error) {
+        console.log('Chrome не удалось запустить, пробуем Firefox...');
+        const firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments('--headless');
+
+        driver = await new Builder()
+            .forBrowser('firefox')
+            .setFirefoxOptions(firefoxOptions)
+            .build();
+        console.log('Firefox запущен успешно!');
+    }
 
     try {
         console.log('Открываю сайт Medium.com...');
